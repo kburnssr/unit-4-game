620d6aa86658d7e216e2d18410b6f9aa4f4ae198
kburnssr <50921973+kburnssr@users.noreply.github.com>
2019-07-14 12:22:47 -0700

    first commit of HOMEWORK-jquery-due-july-16-2019

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		body {
+			background-color: red;
+		}
+	</style>
+</head>
+<body>
+
+<!-- write the word script and hit tab -->
+<script type="text/javascript" src="../images/WordGuess.jpg">
+	//if the line does not end with a curly bracket, you end it with a ;
+	var x = 2; 
+
+	alert(x+1);
+
+	console.log(x-1); //1
+
+</script>
+</body>
+</html>
